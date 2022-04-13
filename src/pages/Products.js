286c962d8b6b6b0a6f6c0e8d5906d1a2062c43f1
tonynnabs <tonynnabs01@gmail.com>
2022-04-13 09:11:06 -0400

    defining and using routes

---
+++ src/pages/Products.js
@@ -0,0 +1,5 @@
+const Products = () => {
+  return <h1>This are my products.</h1>;
+};
+
+export default Products;
---
+++ src/pages/Welcome.js
@@ -0,0 +1,5 @@
+const Welcome = () => {
+  return <h1>Welcome Mr Tony</h1>;
+};
+
+export default Welcome;
